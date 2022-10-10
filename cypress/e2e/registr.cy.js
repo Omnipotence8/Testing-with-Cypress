b69f52c2af71fd4817e3df9859892921ea79b622
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2022-10-10 03:46:18 +0300

    registration constants

--- cypress/e2e/registr.cy.js
+++ cypress/e2e/registr.cy.js
@@ -1,17 +1,37 @@
+const url = 'https://ecommerce-playground.lambdatest.io/index.php?route=account/register';
+const locatorFirstName = '#input-firstname';
+const locatorLastName = '#input-lastname';
+const locatorEmail = '#input-email';
+const locatorTelephone = '#input-telephone';
+const locatorPassword = '#input-password';
+const locatorConfirm = '#input-confirm';
+const locatorYes = '.custom-radio:nth-child(1) > .custom-control-label';
+const locatorNo = '.custom-control:nth-child(2) > .custom-control-label';
+const locatorPrivacy = '.custom-checkbox > .custom-control-label';
+const locatorContinue = '.btn:nth-child(2)';
+const locatorAlert = '#account-register > .alert';
+
+const typeFirstName = 'Mila';
+const typeLastName = 'Kamila';
+const typeEmail = 'myname1010@gmail.com';
+const typeTelephone = '12345543';
+const typePassword = '889977';
+const warning = " Warning: E-Mail Address is already registered!";
+
 describe('Register Account', () => {
-  it('clicking "type" navigates to a new url', () => {
-cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/register');    
-cy.get('#input-firstname').type('Mila');
-cy.get('#input-lastname').type('Kamila');
-cy.get('#input-email').type('myname1010@gmail.com');
-cy.get('#input-telephone').type('12345543');
-cy.get('#input-password').type('889977');
-cy.get('#input-confirm').type('889977');
-cy.get('.custom-radio:nth-child(1) > .custom-control-label').click();
-cy.get('.custom-control:nth-child(2) > .custom-control-label').click();
-cy.get('.custom-checkbox > .custom-control-label').click();
-cy.get('.btn:nth-child(2)').click();
-cy.url().should('contains', 'https://ecommerce-playground.lambdatest.io/index.php');
-cy.get('#account-register > .alert').contains(" Warning: E-Mail Address is already registered!")
+  it('account registration', () => {
+cy.visit(url);    
+cy.get(locatorFirstName).type(typeFirstName);
+cy.get(locatorLastName).type(typeLastName);
+cy.get(locatorEmail).type(typeEmail);
+cy.get(locatorTelephone).type(typeTelephone);
+cy.get(locatorPassword).type(typePassword);
+cy.get(locatorConfirm).type(typePassword);
+cy.get(locatorYes).click();
+cy.get(locatorNo).click();
+cy.get(locatorPrivacy).click();
+cy.get(locatorContinue).click();
+cy.get(locatorAlert).contains(warning)
+cy.location('protocol').should('eq', 'https:')
 })
 })
