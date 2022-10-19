b9e0dc8d100e0a2b81cd4118e489263892676382
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2022-10-19 06:52:07 +0300

    modify cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,9 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
+    baseUrl: 'https://ecommerce-playground.lambdatest.io/index.php',
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
